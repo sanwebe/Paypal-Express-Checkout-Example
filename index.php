9061a28c2b4e67393cc2c416bd830aff896c1d86
Raphaël Dartigues <raphael.dartigues@gmail.com>
2016-01-20 14:16:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ body{font-family: arial;color: #7A7A7A;margin:0px;padding:0px;}
     <input type="hidden" name="itemdesc" value="Capture all your special moments with the Canon EOS Rebel XS/1000D DSLR camera and cherish the memories over and over again." /> 
 	<input type="hidden" name="itemprice" value="225.00" />
     Quantity : <select name="itemQty"><option value="1">1</option><option value="2">2</option><option value="3">3</option></select> 
-    <input class="dw_button" type="submit" name="submitbutt" value="Buy (225.00 <?php echo $PayPalCurrencyCode; ?>)" />
+    <input class="dw_button" type="submit" name="submitbutt" value="Buy (225.00 <?php echo PPL_CURRENCY_CODE; ?>)" />
     </form>
     </td>
   </tr>
@@ -36,7 +36,7 @@ body{font-family: arial;color: #7A7A7A;margin:0px;padding:0px;}
 	<input type="hidden" name="itemnumber" value="20000" /> 
     <input type="hidden" name="itemdesc" value="Nikon Coolpix S9050 26355 digital camera capture vibrant photos up to 12.1 megapixels." /> 
 	<input type="hidden" name="itemprice" value="109.99" /> Quantity : <select name="itemQty"><option value="1">1</option><option value="2">2</option><option value="3">3</option></select> 
-    <input class="dw_button" type="submit" name="submitbutt" value="Buy (109.99 <?php echo $PayPalCurrencyCode; ?>)" />
+    <input class="dw_button" type="submit" name="submitbutt" value="Buy (109.99 <?php echo PPL_CURRENCY_CODE; ?>)" />
     </form></td>
   </tr>
 </table>
